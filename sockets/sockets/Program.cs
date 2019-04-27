cf5b93a9bcef917e19a88694c6c86ee5b3fcfd2e
lazaryan <lazaryan99@gmail.com>
2019-04-27 12:10:38 +0300

    update server

--- sockets/sockets/Program.cs
+++ sockets/sockets/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace sockets
 {
@@ -15,7 +17,12 @@ namespace sockets
         static void Main(string[] args)
         {
             Server();
-            //Client("10.131.56.21", port);
+            //Client("message", "10.131.56.21", port);
+            string mess = CreateMessage("aaa", 123, "asd");
+            string[] aa = getArrayToMessage(mess);
+            Console.WriteLine(aa[1]);
+            Console.WriteLine(mess);
+            Console.ReadKey();
         }
 
         static void Server()
@@ -23,23 +30,62 @@ namespace sockets
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             Console.WriteLine("Server start...");
             socket.Bind(new IPEndPoint(IPAddress.Any, port));
-            socket.Listen(10);
-            socket.Accept();
-            byte[] buffer = new byte[1024];
-            socket.Receive(buffer);
-            Console.WriteLine(Encoding.ASCII.GetString(buffer));
+            socket.Listen(20);
+            while (true)
+            {
+                socket.Accept();
+                byte[] buffer = new byte[1024];
+                int receive = socket.Receive(buffer);
+                string message = Encoding.ASCII.GetString(buffer, 0, receive);
+
+                //string[] arrayMessage = getArrayToMessage(message);
+
+                //if (arrayMessage[0] == "add_user")
+                //{
+                //    add_user(arrayMessage[1], arrayMessage[2]);
+                //}
+                if (message != "finish")
+                {
+                    using (StreamWriter sw = new StreamWriter("tmp/command.txt"))
+                    {
+                        sw.WriteLine(message);
+                    }
+                } else
+                {
+                    Console.WriteLine("aaaa finish!!!!!!!!!!");
+                }
+            }
         }
 
-        static void Client(string ip, int port)
+        static void Client(string message, string ip, int port)
         {
             Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            Console.WriteLine("Enter message:");
             socket.Connect(ip, port);
-            string message = Console.ReadLine();
 
             byte[] buffer = Encoding.ASCII.GetBytes(message);
 
             socket.Send(buffer);
+        }
+
+        static string CreateMessage(string type, params object[] args)
+        {
+            string tmp = type;
+            foreach (object el in args)
+            {
+                tmp += " " + el.ToString();
+            }
+
+            return tmp;
+        }
+
+        static string[] getArrayToMessage(string message)
+        {
+            return message.Split(' ');
+        }
+
+        static void add_user(string ip, string name)
+        {
+            //TODO: add user to game
         }
     }
 }
